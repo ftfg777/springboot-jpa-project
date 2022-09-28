c1cace5d4ea9a461e211fecc81846b718b14cb8f
JeongChanWoo <ftfgz5093@gmail.com>
2022-09-28 18:35:52 +0900

    CODE UP 1036

--- src/main/java/com/godcoder/myrest/Main.java
+++ src/main/java/com/godcoder/myrest/Main.java
@@ -1,14 +1,16 @@
 package com.godcoder.myrest;
+// 코드업 문제 풀이용 main
 
+import jdk.jfr.Unsigned;
 
 import java.util.Scanner;
-// 코드업 문제 풀이용 main
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        char ch1 = scanner.next().charAt(0);
-        char ch2 = scanner.next().charAt(0);
 
-        System.out.println(ch2+" "+ch1);
+        char ch = scanner.next().charAt(0);
+        int num = Integer.parseInt(String.valueOf(ch));
+        System.out.println(String.format("%c", num));
+
     }
 }
